05d6f5e07be36e1c44924ff019c23ce9ec3be7f3
Cooba North <coobanorth@gmail.com>
2026-03-09 17:00:32 +0000

    api edits

--- src/client/js/chats.js
+++ src/client/js/chats.js
@@ -1,4 +1,60 @@
-let user_id = 1;
+window.addEventListener('load', function () {
+    let user_id = 1;
 
-const js_display_id = document.getElementById("display_id");
-js_display_id.textContent = "User ID: "+user_id;
+    const js_display_id = document.getElementById("display_id");
+    js_display_id.textContent = "User ID: " + user_id;
+
+
+    all_chats();
+
+});
+
+async function all_chats() {
+    const url = 'https://cn483.brighton.domains/soundshare/src/server/api.php?chat=all';
+
+    try {
+
+        const response = await fetch(url);
+        const text = await response.text();
+        const obj = JSON.parse(text);
+
+        for (const item of obj.chat) {
+
+const dm_chat_id = item.dm_chat_id;
+const dm_chat_sender = item.dm_chat_sender;
+const dm_chat_receiver = item.dm_chat_receiver;
+const dm_chat_message = item.dm_chat_message;
+const dm_chat_timestamp = item.dm_chat_timestamp;
+
+            const div = document.createElement("div");
+            div.classList.add("chat-card");
+
+            // text
+            const id = document.createElement("p");
+            id.textContent = `Time Of chat: ${dm_chat_id}`;
+
+            const sender = document.createElement("p");
+            sender.textContent = `Sender: ${dm_chat_sender}`;
+
+            const receiver = document.createElement("p");
+            receiver.textContent = `Receiver: ${dm_chat_receiver}`;
+
+            const message = document.createElement("p");
+            message.textContent = `Message: ${dm_chat_message}`;
+
+            const time = document.createElement("p");
+            time.textContent = `Time: ${dm_chat_timestamp}`;
+
+            div.appendChild(id);
+            div.appendChild(sender);
+            div.appendChild(receiver);
+            div.appendChild(message);
+            div.appendChild(time);
+
+            document.getElementById("chat_list").appendChild(div);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+};
--- src/server/api.php
+++ src/server/api.php
@@ -9,7 +9,7 @@ class MyAPI
     function __construct()
     {
         //sets sqli to throw an exception for errors instead of outputting error message
-        mysqli_report(MYSQLI_REPORT_STRICT | MYSQLI_REPORT_STRICT);
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         //sets HTTP header with JSON MIME type
         header('content-type: application/json');
         try {
@@ -37,15 +37,77 @@ class MyAPI
         //handles posts
         if ($method === 'POST') {
 
-        } 
-        
-        //handles gets
-        elseif ($method === 'GET') {
         }
 
-        //if not specified throw error
-        else{
-            http_response_code(405);
+        //handles gets
+        elseif ($method === 'GET') {
+            if (isset($_GET["chat"])) {
+                $chat = $_GET["chat"];
+
+                // Check for the keyword 'all'
+                if ($chat === "all") {
+                    $this->get_all_chats($mysqli);
+                    exit();
+                }
+
+                // Invalid input
+                http_response_code(400);
+                exit();
+            }
+
+            //if not specified throw error
+            else {
+                http_response_code(400);
+            }
+        }
+    }
+
+    //get all chats
+    private function get_all_chats($mysqli)
+    {
+        $sql = "SELECT * FROM dm_chats ORDER BY dm_chat_timestamp DESC";
+        $result = $mysqli->query($sql);
+
+        $this->result_to_json($result);
+    }
+
+    private function result_to_json($result)
+    {
+        //if there is a result
+        if ($result !== false) {
+            //check if there is more than 1 row
+            if ($result->num_rows > 0) {
+
+                //creates json object
+                $myObj = new stdClass();
+                //creates chat array
+                $chat_array = array();
+
+                while ($row = $result->fetch_row()) {
+                    //creating each chat object
+                    $resultclass = new stdClass();
+                    //adding sql result array elements as parameters in the resultclass object
+                    $resultclass->dm_chat_id = (int) $row[0];
+                    $resultclass->dm_chat_sender = $row[1];
+                    $resultclass->dm_chat_receiver = $row[2];
+                    $resultclass->dm_chat_message = $row[3];
+                    $resultclass->dm_chat_timestamp = $row[4];
+                    //adding object to the messages_array array
+                    $chat_array[] = $resultclass;
+                }
+
+                //setting chat parameter as the chat_array array
+                $myObj->chat = $chat_array;
+                $myJSON = json_encode($myObj, JSON_PRETTY_PRINT);
+                echo $myJSON;
+
+                //free memory from storing result set
+                $result->free_result();
+            } else {
+                http_response_code(204);
+            }
+        } else {
+            http_response_code(404);
         }
     }
 
