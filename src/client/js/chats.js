0016656b4bc0d8e078fbd7e77239760516e1770b
Cooba North <coobanorth@gmail.com>
2026-03-12 14:00:22 +0000

    message box
    still need to add function where it is hidden by default and appears when a conversatyion is selected

--- src/client/js/chats.js
+++ src/client/js/chats.js
@@ -4,12 +4,10 @@ window.addEventListener('load', function () {
     const js_display_id = document.getElementById("display_id");
     js_display_id.textContent = "User ID: " + user_id;
 
-
-    all_chats(user_id);
-
+    load_chats(user_id);
 });
 
-async function all_chats(user_id) {
+async function load_chats(user_id) {
     const url = 'https://cn483.brighton.domains/soundshare/src/server/api.php?chat=all';
 
     try {
@@ -19,9 +17,6 @@ async function all_chats(user_id) {
         const obj = JSON.parse(text);
 
         conversation_list(user_id, obj)
-        message_in_a_chat(user_id, obj)
-
-
 
     } catch (error) {
         console.log(error);
@@ -49,6 +44,10 @@ async function conversation_list(user_id, obj) {
         sender.textContent = `Sender: ${x}`;
         sender.id = x;
 
+        sender.addEventListener("click", function () {
+            message_in_a_chat(user_id, obj, this.id);
+        });
+
         div.appendChild(sender);
 
         document.getElementById("chat_list").appendChild(div);
@@ -57,21 +56,33 @@ async function conversation_list(user_id, obj) {
 };
 
 //output messages in chat box
-async function message_in_a_chat(user_id, obj) {
+async function message_in_a_chat(user_id, obj, sender_id) {
+
+    const chatBox = document.getElementById("chat");
+
+    chatBox.innerHTML = "";
+
     for (const item of obj.chat) {
 
+        const dm_chat_sender = item.dm_chat_sender;
         const dm_chat_receiver = item.dm_chat_receiver;
         const dm_chat_message = item.dm_chat_message;
 
+        if (
+            (dm_chat_sender == user_id && dm_chat_receiver == sender_id) ||
+            (dm_chat_sender == sender_id && dm_chat_receiver == user_id)
+        ) {
 
-        const next_message = document.createElement("p");
-        if (dm_chat_receiver == user_id) {
-            next_message.classList.add("received");
+            const next_message = document.createElement("p");
+
+            if (dm_chat_receiver == user_id) {
+                next_message.classList.add("received");
+            } else {
+                next_message.classList.add("sent");
+            }
+
+            next_message.textContent = dm_chat_message;
+            chatBox.appendChild(next_message);
         }
-        else {
-            next_message.classList.add("sent");
-        }
-        next_message.textContent = `${dm_chat_message}`;
-        document.getElementById("chat").appendChild(next_message);
     }
 }
