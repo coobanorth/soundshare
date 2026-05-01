3e1cead53c44df4f6a8bbe1fcdf7e4ca0e2a136a
Cooba North <coobanorth@gmail.com>
2026-05-01 11:28:45 +0100

    manifest

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('./service-worker.js')
             .then(reg => console.log('Service Worker Registered'))
             .catch(err => console.log('Service Worker Registration Failed', err));
     });
